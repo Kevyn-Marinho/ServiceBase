b75ad16ff097000232c8a222751bcf4386a4861c
Kevyn Marinho <devconvets@gmail.com>
2024-02-17 21:15:42 -0300

    save

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.json())
 
-require('./config/mongoose')();
+// require('./config/mongoose')();
 require('./config/routes')(app);
 
 app.get('/', function(req, res) {
